e6629b66d7e1d5317a19c88634034014fde368af
sanghandsome <nguyendanhsanghandsome@gmail.com>
2024-12-04 16:51:01 +0700

    first commit

--- tlunews.sql
+++ tlunews.sql
@@ -1,9 +1,24 @@
 create table users (
-	id INT,
+	id INT PRIMARY KEY AUTO_INCREMENTER,
 	username VARCHAR(255),
 	password VARCHAR(255),
 	role INT CHECK (role IN (0, 1))
 );
+
+CREATE TABLE categrories(
+    id  INT PRIMARY KEY AUTO_INCREMENTER,
+    name VARCHAR(255) NOT NULL
+);
+ 
+CREATE TABLE news(
+ 	 id INT AUTO_INCREMENT PRIMARY KEY, 
+    title VARCHAR(255) NOT NULL,       
+    content TEXT,                      
+    image VARCHAR(255),                
+    create_at Date 
+    category_id INT,                  
+    CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES categories(id)
+);   
 
 insert into users (id, username, PASSWORD,role) VALUES (1,'Brittney Brooksbank', 'qM3&Bu$ID>ZJAnJT',0);
 insert into users (id, username, PASSWORD,role) values (2, 'Currey Bleythin', 'iN4$v@9R*wLS@5',0);

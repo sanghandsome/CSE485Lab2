0f6801e1554a55fed2bd88288f989906bcfead73
sanghandsome <nguyendanhsanghandsome@gmail.com>
2024-12-04 17:14:03 +0700

    first commit

--- tlunews.sql
+++ tlunews.sql
@@ -20,14 +20,13 @@ CREATE TABLE news(
     FOREIGN KEY (category_id) REFERENCES categrories(id)
 );   
 
-insert into users (id, username, PASSWORD,role) VALUES (1,'Brittney Brooksbank', 'qM3&Bu$ID>ZJAnJT',0);
-insert into users (id, username, PASSWORD,role) values (2, 'Currey Bleythin', 'iN4$v@9R*wLS@5',0);
-insert into users (id, username, PASSWORD,role) values (3, 'Wat Sayles', 'dV5"bT_w#',0);
-insert into users (id, username, PASSWORD,role) values (4, 'Kathrine Grishakin', 'sX4*4k47E1e4N?<',0);
-insert into users (id, username, PASSWORD,role) values (5, 'Romy Dauby', 'tQ1,BuHx3_R@q',0);
-insert into users (id, username, PASSWORD,role) values (6, 'Haskel Mechan', 'cX0*4~}z',0);
-insert into users (id, username, PASSWORD,role) VALUES (7, 'admin', 'admin',1);
-insert into users (id, username, PASSWORD,role) VALUES (8, 'sang', '1234',1);
+insert into users (id, username, PASSWORD,role) VALUES (1, 'admin', 'admin',1);
+insert into users (id, username, PASSWORD,role) VALUES (2, 'sang', '1234',1);
+insert into users (id, username, PASSWORD,role) VALUES (3, 'an', '1234',0);
+insert into users (id, username, PASSWORD,role) VALUES (4, 'luat', '1234',0);
+
+INSERT INTO categrories (id , NAME) VALUES (1, )
+
 
 
 
